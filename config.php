47c4fa847da6f19460ee72cdb25ff35f651fa5b3
Vedank99 <51454248+Vedank99@users.noreply.github.com>
2021-04-29 09:29:50 +0530

    Add files via upload

--- config.php
+++ config.php
@@ -1,6 +1,14 @@
 <?php
 
   include 'values.php';
+
+  //The variables in values.php file are
+  //$server
+  //$user
+  //$pass
+  //$database
+  //$root (The root server of the app)
+
   $conn = mysqli_connect($server, $user, $pass, $database);
 
   if(!$conn){
